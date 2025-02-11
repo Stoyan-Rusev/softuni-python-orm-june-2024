0f5dc5a08370f259532292fc40ed7d04bf6ef447
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-02-11 15:39:37 +0200

    Add files via upload

---
+++ 03_migrations_and_django_admin_exercises/caller.py
@@ -0,0 +1,13 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import Shoe
+
+
+print(Shoe.objects.values_list('brand', flat=True).distinct())
+
+# Create queries within functions
